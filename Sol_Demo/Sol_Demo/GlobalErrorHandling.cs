49639048cbc1b9fda71c776fa6483de6b7de53ac
Kishor Naik <kishor.naik011.net@gmail.com>
2023-12-08 09:11:22 +0530

    Code Commit : Code Added

---
+++ Sol_Demo/Sol_Demo/GlobalErrorHandling.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Demo
+{
+    public sealed class GlobalErrorHandling : IExceptionHandler
+    {
+        private readonly ILogger<GlobalErrorHandling> logger;
+
+        public GlobalErrorHandling(ILogger<GlobalErrorHandling> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            logger.LogError(
+            exception, "Exception occurred: {Message}", exception.Message);
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = httpContext.Response.StatusCode,
+                Title = "Server error",
+                Detail = exception.Message
+            };
+
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken).ConfigureAwait(false);
+
+            return true;
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Program.cs
@@ -0,0 +1,30 @@
+using Sol_Demo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddExceptionHandler<GlobalErrorHandling>();
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseExceptionHandler();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
